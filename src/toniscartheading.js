7a8677d65aa40ab86d0c3f8305ba6b313ea96f98
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-10-05 15:55:53 +0100

    simple state experiment changes colour on click

--- src/toniscartheading.js
+++ src/toniscartheading.js
@@ -1,12 +1,66 @@
 var TonisHeading = React.createClass({
+
+
+  colourChars: {
+    0: 'a',
+    1: 'b',
+    2: 'c',
+    3: 'd',
+    4: 'e',
+    5: 'f'
+  },
+
+
+  getInitialState: function() {
+    return {
+      h1Colour: {
+        backgroundColor: '#fcd'
+      },
+      h2Colour: {
+        backgroundColor: '#dcf'
+      }
+    }
+  },
+
+
+  handleH1Click: function(e) {
+    this.setState({
+      h1Colour: {
+        backgroundColor: this.randomColour()
+      }
+    });
+  },
+
+
+  handleH2Click: function(e) {
+    this.setState({
+      h2Colour: {
+        backgroundColor: this.randomColour()
+      }
+    });
+  },
+
+
+  randomColour: function() {
+    return [
+      '#',
+      this.colourChars[Math.floor(Math.random() * 6)],
+      this.colourChars[Math.floor(Math.random() * 6)],
+      this.colourChars[Math.floor(Math.random() * 6)]
+    ].join('');
+  },
+
+
   render: function() {
     return (
       <div>
-        <h1>Welcome to {this.props.name} Cart</h1>
-        <h2>My first react Component set</h2>
+        <h1 style={this.state.h1Colour} onClick={this.handleH1Click}>Welcome to {this.props.name} Cart</h1>
+        <h2 style={this.state.h2Colour} onClick={this.handleH2Click}>My first react Component set</h2>
       </div>
     );
   }
+
+
 });
 
 React.render(
