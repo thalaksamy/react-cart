9a3c8bb0dcfe82eea9c776f4c80ce507dd042830
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-10-05 14:54:37 +0100

    adds a property

--- src/toniscartheading.js
+++ src/toniscartheading.js
@@ -2,7 +2,7 @@ var TonisHeading = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>Welcome to Tonis Cart</h1>
+        <h1>Welcome to {this.props.name} Cart</h1>
         <h2>My first react Component set</h2>
       </div>
     );
@@ -10,6 +10,6 @@ var TonisHeading = React.createClass({
 });
 
 React.render(
-  <TonisHeading />,
+  <TonisHeading name='Tonis'/>,
   document.getElementById('heading')
 );
