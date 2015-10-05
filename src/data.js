22363e6c634dfde9a5cd9cbcf696cb73d72deab8
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-10-05 18:53:36 +0100

    add some basic data

---
+++ src/data.js
@@ -0,0 +1,27 @@
+var cartItems = [
+  {
+    productName: 'Apple',
+    quantityBagged: 3,
+    unitPrice: 0.45
+  },
+  {
+    productName: 'Banana',
+    quantityBagged: 1,
+    unitPrice: 0.35
+  },
+  {
+    productName: 'Parsnip',
+    quantityBagged: 1,
+    unitPrice: 0.50
+  },
+  {
+    productName: 'Potato',
+    quantityBagged: 10,
+    unitPrice: 0.10
+  },
+  {
+    productName: 'Honey Pot',
+    quantityBagged: 2,
+    unitPrice: 3.50
+  }
+];
