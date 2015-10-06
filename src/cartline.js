f4c87bae9b0dea470760390d6890eb95b975e0f9
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-10-06 08:49:41 +0100

    removes the bad attempt at interaction from cartline

--- src/cartline.js
+++ src/cartline.js
@@ -12,6 +12,10 @@ var CartLine = React.createClass({
         <div className='cart-line__name'>{this.props.name}</div>
         <div className='cart-line__quantity'>{this.props.qty}</div>
         <div className='cart-line__price'>{this.props.price}</div>
+        <div className='cart-line__change-qty'>
+          <a className='cart-line__qty-up'>+</a>
+          <a className='cart-line__qty-down'>-</a>
+        </div>
         <div className='cart-line__total'>{this.calculateTotal()}</div>
       </div>
     );
